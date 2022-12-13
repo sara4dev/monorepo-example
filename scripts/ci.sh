79a15be1512c2e5e73c18cd485c6ac9ad6e1f0fc
Saravana Periyasamy <saravanakumar.periyasamy@gmail.com>
2022-12-13 02:40:13 +0000

    cleanup the script

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -4,21 +4,17 @@ set -euxo
 # Go to the root of the repo
 cd "$(git rev-parse --show-toplevel)"
 
-git --version
-git log
-
 # Get a list of the current files in package form by querying Bazel.
 if [[ ! -z $GITHUB_BASE_REF ]]; then
   changedFiles=$(git diff --name-only origin/main)
 else
-  changedFiles=$(git diff --name-only $GITHUB_SHA~1)
+  changedFiles=$(git diff --name-only HEAD~1)
 fi
 
-echo $changedFiles
+echo "Changed FIles: $changedFiles"
 
 files=()
 for file in $changedFiles; do
-  echo $file
   if [[ $file = src/* ]]
   then
     files+=($(bazelisk query $file))
@@ -27,23 +23,24 @@ for file in $changedFiles; do
 done
 
 
-# Query for the associated buildImages
+
+echo "Query for the associated buildImages"
 buildImages=$(bazelisk query \
     --keep_going \
     "filter(.*_image$, rdeps(//..., set(${files[*]})))")    
 
-# Build the docker images only for the changed services
+echo "Build the docker images only for the changed services"
 if [[ ! -z $buildImages ]]; then
   echo "Building docker images"
   bazelisk build $buildImages
 fi
 
-# Query for the associated Images to push
+echo "Query for the associated Images to push"
 pushImages=$(bazelisk query \
     --keep_going \
     "kind(container_push, rdeps(//..., set(${files[*]})))")
 
-# Push the docker images only for the changed services
+echo "Push the docker images only for the changed services"
 if [[ ! -z $pushImages ]]; then
   echo "Pushing docker images"
   bazelisk run $pushImages
